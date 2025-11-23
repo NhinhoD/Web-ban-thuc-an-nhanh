01e5887a288f8517f79983d22185603e3adfcad6
NhinhoD <nhinhovippro1977@gmail.com>
2025-11-23 19:53:32 +0700

    update-products

--- obj/Debug/net8.0/ASM_C#4.AssemblyInfo.cs
+++ obj/Debug/net8.0/ASM_C#4.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASM_C_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+665c0d84bdff230399255cbd13d0164877e821de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+135a5bf80bf5845bf22226dc179be49150e529a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASM_C_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASM_C_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
